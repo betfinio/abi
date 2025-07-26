c843fc815926e12591f2dde4dcb57b0eb6be9c26
BF san <bf-san@proton.me>
2025-07-26 20:13:49 +0200

    feat: add export for Metadata contract and restore RankNFT export in index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -48,3 +48,4 @@ export * from "./contracts/MultiBet.ts";
 export * from "./contracts/LostTicketsClaimer.ts";
 export * from "./contracts/Rank.ts";
 export * from "./contracts/RankNFT.ts";
+export * from "./contracts/Metadata.ts";
